89a55fa7196c30ad500ce0d17d2b0f732dcbcfd4
lvtu0316 <550407948@qq.com>
2020-01-17 10:06:14 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # lottery
-会场抽奖小系统
+## 会场抽奖小系统
+
+### 安装和使用：
+
+本程序基于 [fouber/lottery](https://github.com/fouber/lottery ) 进行的扩展。
+
+系统使用 vue + tagcanvas.js 开发，直接下载程序，修改 js 下的 member.js文件，录入参与抽奖人员名单图片，
+
+浏览器直接打开 index.html 即可使用。
+
+退格键（backspace) 可调出选择一次抽奖人数，再次按键隐藏。
+
+P 按键可播放背景音乐，再次按键暂停播放。
+
+系统默认使用tagcanvas 3D球形展示，去掉 `// shape: "vring(0.5)",` 的注释可改为水平环状图形。更多的图形展示可查看 [tagcanvas]( https://www.goat1000.com/tagcanvas-functions.php) 相关使用文档。
+
+
+
---
+++ js/member.js
@@ -0,0 +1,527 @@
+var member = [
+  {
+    "num": "No.01",
+    "name": "ade",
+    "img": "img/file/ade.jpg"
+  },
+  {
+    "num": "No.02",
+    "name": "bob",
+    "img": "img/file/bob.png"
+  },
+  {
+    "num": "No.03",
+    "name": "christian",
+    "img": "img/file/christian.jpg"
+  },
+  {
+    "num": "No.04",
+    "name": "daniel",
+    "img": "img/file/daniel.jpg"
+  },
+  {
+    "num": "No.05",
+    "name": "elliot",
+    "img": "img/file/elliot.jpg"
+  },
+  {
+    "num": "No.06",
+    "name": "elyse",
+    "img": "img/file/elyse.png"
+  },
+  {
+    "num": "No.07",
+    "name": "eve",
+    "img": "img/file/eve.png"
+  },
+  {
+    "num": "No.08",
+    "name": "hebe",
+    "img": "img/file/hebe.jpg"
+  },
+  {
+    "num": "No.09",
+    "name": "helen",
+    "img": "img/file/helen.jpg"
+  },
+  {
+    "num": "No.10",
+    "name": "jenny",
+    "img": "img/file/jenny.jpg"
+  },
+  {
+    "num": "No.11",
+    "name": "joe",
+    "img": "img/file/joe.jpg"
+  },
+  {
+    "num": "No.12",
+    "name": "justen",
+    "img": "img/file/justen.jpg"
+  },
+  {
+    "num": "No.13",
+    "name": "kristy",
+    "img": "img/file/kristy.png"
+  },
+  {
+    "num": "No.14",
+    "name": "laura",
+    "img": "img/file/laura.jpg"
+  },
+  {
+    "num": "No.15",
+    "name": "matt",
+    "img": "img/file/matt.jpg"
+  },
+  {
+    "num": "No.16",
+    "name": "matthew",
+    "img": "img/file/matthew.png"
+  },
+  {
+    "num": "No.17",
+    "name": "molly",
+    "img": "img/file/molly.png"
+  },
+  {
+    "num": "No.18",
+    "name": "nom",
+    "img": "img/file/nom.jpg"
+  },
+  {
+    "num": "No.19",
+    "name": "nan",
+    "img": "img/file/nan.jpg"
+  },
+  {
+    "num": "No.20",
+    "name": "patrick",
+    "img": "img/file/patrick.png"
+  },
+  {
+    "num": "No.21",
+    "name": "rachel",
+    "img": "img/file/rachel.png"
+  },
+  {
+    "num": "No.22",
+    "name": "steve",
+    "img": "img/file/steve.jpg"
+  },
+  {
+    "num": "No.23",
+    "name": "stevie",
+    "img": "img/file/stevie.jpg"
+  },
+  {
+    "num": "No.24",
+    "name": "tom",
+    "img": "img/file/tom.jpg"
+  },
+  {
+    "num": "No.25",
+    "name": "veronika",
+    "img": "img/file/veronika.jpg"
+  },
+  {
+    "num": "No.26",
+    "name": "zoe",
+    "img": "img/file/zoe.jpg"
+  },
+  {
+    "num": "No.01",
+    "name": "ade",
+    "img": "img/file/ade.jpg"
+  },
+  {
+    "num": "No.02",
+    "name": "bob",
+    "img": "img/file/bob.png"
+  },
+  {
+    "num": "No.03",
+    "name": "christian",
+    "img": "img/file/christian.jpg"
+  },
+  {
+    "num": "No.04",
+    "name": "daniel",
+    "img": "img/file/daniel.jpg"
+  },
+  {
+    "num": "No.05",
+    "name": "elliot",
+    "img": "img/file/elliot.jpg"
+  },
+  {
+    "num": "No.06",
+    "name": "elyse",
+    "img": "img/file/elyse.png"
+  },
+  {
+    "num": "No.07",
+    "name": "eve",
+    "img": "img/file/eve.png"
+  },
+  {
+    "num": "No.08",
+    "name": "hebe",
+    "img": "img/file/hebe.jpg"
+  },
+  {
+    "num": "No.09",
+    "name": "helen",
+    "img": "img/file/helen.jpg"
+  },
+  {
+    "num": "No.10",
+    "name": "jenny",
+    "img": "img/file/jenny.jpg"
+  },
+  {
+    "num": "No.11",
+    "name": "joe",
+    "img": "img/file/joe.jpg"
+  },
+  {
+    "num": "No.12",
+    "name": "justen",
+    "img": "img/file/justen.jpg"
+  },
+  {
+    "num": "No.13",
+    "name": "kristy",
+    "img": "img/file/kristy.png"
+  },
+  {
+    "num": "No.14",
+    "name": "laura",
+    "img": "img/file/laura.jpg"
+  },
+  {
+    "num": "No.15",
+    "name": "matt",
+    "img": "img/file/matt.jpg"
+  },
+  {
+    "num": "No.16",
+    "name": "matthew",
+    "img": "img/file/matthew.png"
+  },
+  {
+    "num": "No.17",
+    "name": "molly",
+    "img": "img/file/molly.png"
+  },
+  {
+    "num": "No.18",
+    "name": "nom",
+    "img": "img/file/nom.jpg"
+  },
+  {
+    "num": "No.19",
+    "name": "nan",
+    "img": "img/file/nan.jpg"
+  },
+  {
+    "num": "No.20",
+    "name": "patrick",
+    "img": "img/file/patrick.png"
+  },
+  {
+    "num": "No.21",
+    "name": "rachel",
+    "img": "img/file/rachel.png"
+  },
+  {
+    "num": "No.22",
+    "name": "steve",
+    "img": "img/file/steve.jpg"
+  },
+  {
+    "num": "No.23",
+    "name": "stevie",
+    "img": "img/file/stevie.jpg"
+  },
+  {
+    "num": "No.24",
+    "name": "tom",
+    "img": "img/file/tom.jpg"
+  },
+  {
+    "num": "No.25",
+    "name": "veronika",
+    "img": "img/file/veronika.jpg"
+  },
+  {
+    "num": "No.26",
+    "name": "zoe",
+    "img": "img/file/zoe.jpg"
+  },
+  {
+    "num": "No.01",
+    "name": "ade",
+    "img": "img/file/ade.jpg"
+  },
+  {
+    "num": "No.02",
+    "name": "bob",
+    "img": "img/file/bob.png"
+  },
+  {
+    "num": "No.03",
+    "name": "christian",
+    "img": "img/file/christian.jpg"
+  },
+  {
+    "num": "No.04",
+    "name": "daniel",
+    "img": "img/file/daniel.jpg"
+  },
+  {
+    "num": "No.05",
+    "name": "elliot",
+    "img": "img/file/elliot.jpg"
+  },
+  {
+    "num": "No.06",
+    "name": "elyse",
+    "img": "img/file/elyse.png"
+  },
+  {
+    "num": "No.07",
+    "name": "eve",
+    "img": "img/file/eve.png"
+  },
+  {
+    "num": "No.08",
+    "name": "hebe",
+    "img": "img/file/hebe.jpg"
+  },
+  {
+    "num": "No.09",
+    "name": "helen",
+    "img": "img/file/helen.jpg"
+  },
+  {
+    "num": "No.10",
+    "name": "jenny",
+    "img": "img/file/jenny.jpg"
+  },
+  {
+    "num": "No.11",
+    "name": "joe",
+    "img": "img/file/joe.jpg"
+  },
+  {
+    "num": "No.12",
+    "name": "justen",
+    "img": "img/file/justen.jpg"
+  },
+  {
+    "num": "No.13",
+    "name": "kristy",
+    "img": "img/file/kristy.png"
+  },
+  {
+    "num": "No.14",
+    "name": "laura",
+    "img": "img/file/laura.jpg"
+  },
+  {
+    "num": "No.15",
+    "name": "matt",
+    "img": "img/file/matt.jpg"
+  },
+  {
+    "num": "No.16",
+    "name": "matthew",
+    "img": "img/file/matthew.png"
+  },
+  {
+    "num": "No.17",
+    "name": "molly",
+    "img": "img/file/molly.png"
+  },
+  {
+    "num": "No.18",
+    "name": "nom",
+    "img": "img/file/nom.jpg"
+  },
+  {
+    "num": "No.19",
+    "name": "nan",
+    "img": "img/file/nan.jpg"
+  },
+  {
+    "num": "No.20",
+    "name": "patrick",
+    "img": "img/file/patrick.png"
+  },
+  {
+    "num": "No.21",
+    "name": "rachel",
+    "img": "img/file/rachel.png"
+  },
+  {
+    "num": "No.22",
+    "name": "steve",
+    "img": "img/file/steve.jpg"
+  },
+  {
+    "num": "No.23",
+    "name": "stevie",
+    "img": "img/file/stevie.jpg"
+  },
+  {
+    "num": "No.24",
+    "name": "tom",
+    "img": "img/file/tom.jpg"
+  },
+  {
+    "num": "No.25",
+    "name": "veronika",
+    "img": "img/file/veronika.jpg"
+  },
+  {
+    "num": "No.26",
+    "name": "zoe",
+    "img": "img/file/zoe.jpg"
+  },
+  {
+    "num": "No.01",
+    "name": "ade",
+    "img": "img/file/ade.jpg"
+  },
+  {
+    "num": "No.02",
+    "name": "bob",
+    "img": "img/file/bob.png"
+  },
+  {
+    "num": "No.03",
+    "name": "christian",
+    "img": "img/file/christian.jpg"
+  },
+  {
+    "num": "No.04",
+    "name": "daniel",
+    "img": "img/file/daniel.jpg"
+  },
+  {
+    "num": "No.05",
+    "name": "elliot",
+    "img": "img/file/elliot.jpg"
+  },
+  {
+    "num": "No.06",
+    "name": "elyse",
+    "img": "img/file/elyse.png"
+  },
+  {
+    "num": "No.07",
+    "name": "eve",
+    "img": "img/file/eve.png"
+  },
+  {
+    "num": "No.08",
+    "name": "hebe",
+    "img": "img/file/hebe.jpg"
+  },
+  {
+    "num": "No.09",
+    "name": "helen",
+    "img": "img/file/helen.jpg"
+  },
+  {
+    "num": "No.10",
+    "name": "jenny",
+    "img": "img/file/jenny.jpg"
+  },
+  {
+    "num": "No.11",
+    "name": "joe",
+    "img": "img/file/joe.jpg"
+  },
+  {
+    "num": "No.12",
+    "name": "justen",
+    "img": "img/file/justen.jpg"
+  },
+  {
+    "num": "No.13",
+    "name": "kristy",
+    "img": "img/file/kristy.png"
+  },
+  {
+    "num": "No.14",
+    "name": "laura",
+    "img": "img/file/laura.jpg"
+  },
+  {
+    "num": "No.15",
+    "name": "matt",
+    "img": "img/file/matt.jpg"
+  },
+  {
+    "num": "No.16",
+    "name": "matthew",
+    "img": "img/file/matthew.png"
+  },
+  {
+    "num": "No.17",
+    "name": "molly",
+    "img": "img/file/molly.png"
+  },
+  {
+    "num": "No.18",
+    "name": "nom",
+    "img": "img/file/nom.jpg"
+  },
+  {
+    "num": "No.19",
+    "name": "nan",
+    "img": "img/file/nan.jpg"
+  },
+  {
+    "num": "No.20",
+    "name": "patrick",
+    "img": "img/file/patrick.png"
+  },
+  {
+    "num": "No.21",
+    "name": "rachel",
+    "img": "img/file/rachel.png"
+  },
+  {
+    "num": "No.22",
+    "name": "steve",
+    "img": "img/file/steve.jpg"
+  },
+  {
+    "num": "No.23",
+    "name": "stevie",
+    "img": "img/file/stevie.jpg"
+  },
+  {
+    "num": "No.24",
+    "name": "tom",
+    "img": "img/file/tom.jpg"
+  },
+  {
+    "num": "No.25",
+    "name": "veronika",
+    "img": "img/file/veronika.jpg"
+  },
+  {
+    "num": "No.26",
+    "name": "zoe",
+    "img": "img/file/zoe.jpg"
+  },
+  
+  
+
+
+  
+]
